5b4f8b03fa52bb52a9cc586a1d467abed9b795f8
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2024-02-27 10:16:58 -0500

    best time to buy and sell stock

--- neet_code/sliding_window/best_time_to_buy_and_sell_stock.cpp
+++ neet_code/sliding_window/best_time_to_buy_and_sell_stock.cpp
@@ -1,19 +1,41 @@
+#include <iostream>
 #include <vector>
 
-class Solution {
+class Solution1{
 public:
   int maxProfit(std::vector<int>& prices){
     int max_profit = 0;
 
     for(int i = 0; i < prices.size(); ++i){
-      
+      for(int j = i + 1; j < prices.size(); ++j){
+        max_profit = std::max(prices[j] - prices[i], max_profit);
+      }
     }
+
+    return max_profit;
+  }
+};
+
+class Solution2{
+public:
+  int maxProfit(std::vector<int>& prices){
+    int min_start = prices[0];
+    int max_profit = 0;
+
+    for(int i = 1; i < prices.size(); ++i){
+      max_profit = std::max(prices[i] - min_start, max_profit);
+      min_start  = std::min(prices[i], min_start);
+    }
+
     return max_profit;
   }
 };
 
 int main(){
   std::vector<int> prices = {7, 1, 5, 3, 6, 4};
-  auto result = Solution().maxProfit(prices);
+  auto result = Solution2().maxProfit(prices);
+
+  std::cout << result << std::endl;
+
   return 0;
 }
